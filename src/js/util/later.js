1f56c0a1d908a7d966a103e5179ac9c740653064
Lars Thorup <lars@zealake.com>
2014-11-08 08:04:34 +0100

    mock clock

--- src/js/util/later.js
+++ src/js/util/later.js
@@ -1,23 +1,26 @@
-    /*global window, jQuery*/
-    (function (window, $) {
-        'use strict';
+/*global window, jQuery*/
+(function (window, $) {
+    'use strict';
 
-        function add(a, b, cb) {
-            window.setTimeout(function () {
-                cb(null, a + b);
-            }, 100);
-        }
+    var delay = 100;
 
-        function adding(a, b) {
-            var deferred = $.Deferred();
-            window.setTimeout(function () {
-                deferred.resolve(a + b);
-            }, 100);
-            return deferred.promise();
-        }
+    function add(a, b, cb) {
+        window.setTimeout(function () {
+            cb(null, a + b);
+        }, delay);
+    }
 
-        window.later = {
-            add: add,
-            adding: adding
-        };
-    })(window, jQuery);
+    function adding(a, b) {
+        var deferred = $.Deferred();
+        window.setTimeout(function () {
+            deferred.resolve(a + b);
+        }, delay);
+        return deferred.promise();
+    }
+
+    window.later = {
+        delay: delay,
+        add: add,
+        adding: adding
+    };
+})(window, jQuery);
--- test/js/util/later.test.js
+++ test/js/util/later.test.js
@@ -1,8 +1,8 @@
-/*global describe, it, expect, window */
+/*global jasmine, describe, beforeEach, afterEach, it, expect, window */
 describe('util.later', function () {
     'use strict';
 
-    describe('add', function () {
+    describe('add, slow', function () {
 
         it('should add correctly', function (done) {
             window.later.add(2, 3, function (err, sum) {
@@ -14,13 +14,34 @@ describe('util.later', function () {
 
     });
 
-    describe('adding', function () {
+    describe('adding, slow', function () {
 
         it('should add correctly', function (done) {
             var adding = window.later.adding(2, 3);
             adding.then(function (sum) {
                 expect(sum).toBe(5);
                 done();
+            });
+        });
+
+    });
+
+    describe('adding, fast', function () {
+
+        beforeEach(function () {
+            jasmine.clock().install();
+        });
+
+        afterEach(function () {
+            jasmine.clock().uninstall();
+        });
+
+        it('should add correctly', function () {
+            var adding = window.later.adding(2, 3);
+            jasmine.clock().tick(window.later.delay);
+            adding.then(function (sum) {
+                expect(sum).toBe(5);
+                // Note: no longer asynchronous(!)
             });
         });
 
