081c0b77258c971220048b5e9597e3c96900829c
Lars Thorup <lars@zealake.com>
2014-11-08 07:56:27 +0100

    mock date

---
+++ src/js/model/blogpost.js
@@ -0,0 +1,15 @@
+/*global window*/
+(function (window) {
+    'use strict';
+
+    window.BlogPost = function (options) {
+        this.options = options;
+        this.createdOn = new Date();
+    };
+
+    window.BlogPost.prototype.age = function () {
+        var now = new Date();
+        return now.getTime() - this.createdOn.getTime();
+    };
+
+})(window);
---
+++ test/js/model/blogpost.test.js
@@ -0,0 +1,27 @@
+/*global window, describe, beforeEach, afterEach, it, expect, jasmine*/
+describe('BlogPost', function () {
+    'use strict';
+
+    var post;
+
+    beforeEach(function () {
+        jasmine.clock().install();
+        jasmine.clock().mockDate(new Date(2014, 11, 13));
+        post = new window.BlogPost({title: 'better testing'});
+    });
+
+    afterEach(function () {
+        jasmine.clock().uninstall();
+    });
+
+    describe('age', function () {
+
+        beforeEach(function () {
+            jasmine.clock().tick(42000);
+        });
+
+        it('compute age correctly', function () {
+            expect(post.age()).toBe(42000);
+        });
+    });
+});
