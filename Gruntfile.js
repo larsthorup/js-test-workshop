7b6a6211a9fc9913ab386c6ea2f69b1c0ae2d8bc
Lars Thorup <lars@zealake.com>
2014-11-29 16:05:41 -0800

    travis support

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
     grunt.registerTask('all', ['clean', 'lint', 'test']);
 
     // continuous integration
-    grunt.registerTask('ci', ['lint', 'cover']);
+    grunt.registerTask('ci', ['lint', 'test']);
 
 
     // clean
